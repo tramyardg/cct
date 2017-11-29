dd0a70c3f97c7f1ca8e279f019ebf7c0acd91868
Raymart De Guzman <raymart54@gmail.com>
2017-11-29 02:35:07 -0500

    timer in progress

--- js/app.js
+++ js/app.js
@@ -1,10 +1,22 @@
 $(document).ready(function () {
-  testOptions.onSubmitTestOptions('#test-options-form')
+  testOptions.setTestOptionsConstruct('#test-options-form')
+  testOptions.onSubmitTestOptions()
+  timer.setTimerConstruct('#timer-box', '#seconds-span', '#minutes-span')
 })
+const MINUTES_CONSTANT = 60
+const INCREMENT_SECONDS_BY_1000 = 1000
+const HIDE_ELEMENT_CLASS = 'text-hide'
 const testOptions = {
   isTimerSet: false,
   minutes: null,
-  numberOfQuestions: 10,
+  numberOfQuestions: null,
+  testOptionsFormHTML: null,
+  setTestOptionsConstruct (tofHTML) {
+    testOptions.testOptionsFormHTML = tofHTML
+  },
+  hideTestOptionsForm () {
+    $(testOptions.testOptionsFormHTML).addClass(HIDE_ELEMENT_CLASS)
+  },
   setIsTimerSet (cond) {
     if (cond === true || cond === 'true') {
       testOptions.isTimerSet = true
@@ -43,7 +55,8 @@ const testOptions = {
   setNumberOfQuestions (num) {
     testOptions.numberOfQuestions = num
   },
-  onSubmitTestOptions: function (formId) {
+  onSubmitTestOptions: function () {
+    let formId = testOptions.testOptionsFormHTML
     $(formId).submit(function (event) {
       let values = []
       $(this).serializeArray().forEach((element) => {
@@ -53,12 +66,53 @@ const testOptions = {
       testOptions.setIsTimerSet(values[1].value)
       testOptions.setMinutesByNumQuestions(values[0].value)
       testOptions.customToString()
+      testOptions.runTimer()
       event.preventDefault()
     })
   },
-  customToString () {
+  customToString () { // for debugging purposes
     console.log('is timer set ' + testOptions.isTimerSet)
     console.log('number of minutes ' + testOptions.minutes)
     console.log('number of questions ' + testOptions.numberOfQuestions)
+  },
+  runTimer () {
+    if (testOptions.isTimerSet) {
+      timer.displayTimer()
+      timer.setSecondsByMinutes(testOptions.minutes)
+      timer.startTimer()
+    } else {
+      timer.hideTimer()
+    }
+  }
+}
+const timer = {
+  seconds: null,
+  timerHTML: null,
+  secondsHTML: null,
+  minutesHTML: null,
+  setTimerConstruct (timerBox, secHTML, minHTML) {
+    timer.timerHTML = timerBox
+    timer.secondsHTML = secHTML
+    timer.minutesHTML = minHTML
+  },
+  displayTimer () {
+    $(timer.timerHTML).removeClass(HIDE_ELEMENT_CLASS)
+  },
+  hideTimer () {
+    $(timer.timerHTML).addClass(HIDE_ELEMENT_CLASS)
+  },
+  setSecondsByMinutes (passedMinutes) {
+    timer.seconds = passedMinutes * MINUTES_CONSTANT
+  },
+  startTimer () {
+    // let remainingMinutes
+    // let remainingSeconds
+    // let counter = timer.seconds
+    setInterval(function () {
+      // counter--
+      // if (counter >= 0) {
+      //
+      // }
+    }, INCREMENT_SECONDS_BY_1000)
   }
 }
