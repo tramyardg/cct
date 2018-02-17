f512fd8ab3129b1171508fc2aafed74b56e5fb0b
Raymart <tramyardg@gmail.com>
2018-02-17 00:38:35 -0500

    adjusted php code style indention

--- model/connection.php
+++ model/connection.php
@@ -3,25 +3,25 @@
 class Db
 {
 
-    private static $instance = NULL;
+  private static $instance = NULL;
 
-    private function __construct()
-    {
+  private function __construct()
+  {
 
+  }
+
+  private function __close()
+  {
+
+  }
+
+  public static function getInstance()
+  {
+    if (!isset(self::$instance)) {
+      $pdo_options[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;
+      self::$instance = new PDO(
+        'mysql:host=localhost;dbname=canadacitizenshipquiz', 'root', '', $pdo_options);
     }
-
-    private function __close()
-    {
-
-    }
-
-    public static function getInstance()
-    {
-        if (!isset(self::$instance)) {
-            $pdo_options[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;
-            self::$instance = new PDO(
-                'mysql:host=localhost;dbname=canadacitizenshipquiz', 'root', '', $pdo_options);
-        }
-        return self::$instance;
-    }
+    return self::$instance;
+  }
 }
