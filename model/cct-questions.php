d396606b0886b8b8dc7e67a449ba840ddc71d520
Raymart <tramyardg@gmail.com>
2018-02-17 00:44:39 -0500

    adjusted php code style indention 3

--- model/cct-questions.php
+++ model/cct-questions.php
@@ -3,59 +3,59 @@
 require_once 'connection.php';
 
 if (isset($_GET['regionId']) && isset($_GET['limit']) && $_GET['dateTime']) {
-  $regionId = filter_input(INPUT_GET, 'regionId');
-  $limit = filter_input(INPUT_GET, 'limit');
-  $result = querySelectedOptions($regionId, $limit);
+   $regionId = filter_input(INPUT_GET, 'regionId');
+   $limit = filter_input(INPUT_GET, 'limit');
+   $result = querySelectedOptions($regionId, $limit);
 
-  // for special characters
-  header('Content-Type: text/html; charset=ISO-8859-1');
-  $xmlData = "<?xml version=\"1.0\" encoding=\"ISO-8859-1\" ?>\n";
+   // for special characters
+   header('Content-Type: text/html; charset=ISO-8859-1');
+   $xmlData = "<?xml version=\"1.0\" encoding=\"ISO-8859-1\" ?>\n";
 
-  // create an xml to be parsed in ajax
-  $rootElementName = 'QUESTIONS';
-  $childElementName = 'ITEM';
-  $xmlData .= "<" . $rootElementName . ">";
+   // create an xml to be parsed in ajax
+   $rootElementName = 'QUESTIONS';
+   $childElementName = 'ITEM';
+   $xmlData .= "<" . $rootElementName . ">";
 
-  // you can use $col[name] for dynamically unassigned columns
-  // you don't know the name of the column
-  foreach ($result->fetchAll() as $data) {
-    $xmlData .= "<" . $childElementName . ">";
-    for ($i = 0; $i < $result->columnCount(); $i++) {
-      $col = $result->getColumnMeta($i);
-      $xmlData .= "<" . strtoupper($col['name']) . ">";
-      if ($col['name'] == 'diagram' && strlen($data['diagramId']) > 0) {
-        $xmlData .= base64_encode($data['diagram']);
-      } else if (!empty($data[$col['name']])) {
-        $xmlData .= $data[$col['name']];
-      } else if (empty($data[$col['name']])) {
-        $xmlData .= 'null';
+   // you can use $col[name] for dynamically unassigned columns
+   // you don't know the name of the column
+   foreach ($result->fetchAll() as $data) {
+      $xmlData .= "<" . $childElementName . ">";
+      for ($i = 0; $i < $result->columnCount(); $i++) {
+         $col = $result->getColumnMeta($i);
+         $xmlData .= "<" . strtoupper($col['name']) . ">";
+         if ($col['name'] == 'diagram' && strlen($data['diagramId']) > 0) {
+            $xmlData .= base64_encode($data['diagram']);
+         } else if (!empty($data[$col['name']])) {
+            $xmlData .= $data[$col['name']];
+         } else if (empty($data[$col['name']])) {
+            $xmlData .= 'null';
+         }
+         $xmlData .= "</" . strtoupper($col['name']) . ">";
       }
-      $xmlData .= "</" . strtoupper($col['name']) . ">";
-    }
-    $xmlData .= "</" . $childElementName . ">";
-  }
-  $xmlData .= "</" . $rootElementName . ">";
-  print $xmlData;
+      $xmlData .= "</" . $childElementName . ">";
+   }
+   $xmlData .= "</" . $rootElementName . ">";
+   print $xmlData;
 } else {
-  echo 'error';
+   echo 'error';
 }
 
 function querySelectedOptions($regionId, $limit)
 {
-  $sql = "SELECT\n"
-    . " *\n"
-    . "FROM\n"
-    . " quizquestion\n"
-    . "LEFT OUTER JOIN\n"
-    . " quizdiagram ON(\n"
-    . " quizquestion.diagramId = quizdiagram.diagramId\n"
-    . " )\n"
-    . "WHERE\n"
-    . " regionId = '$regionId'\n"
-    . "ORDER BY RAND\n"
-    . " ()\n"
-    . "LIMIT $limit";
-  $dbh = Db::getInstance();
-  $result = $dbh->query($sql);
-  return $result;
+   $sql = "SELECT\n"
+      . " *\n"
+      . "FROM\n"
+      . " quizquestion\n"
+      . "LEFT OUTER JOIN\n"
+      . " quizdiagram ON(\n"
+      . " quizquestion.diagramId = quizdiagram.diagramId\n"
+      . " )\n"
+      . "WHERE\n"
+      . " regionId = '$regionId'\n"
+      . "ORDER BY RAND\n"
+      . " ()\n"
+      . "LIMIT $limit";
+   $dbh = Db::getInstance();
+   $result = $dbh->query($sql);
+   return $result;
 }
--- model/cct-results.php
+++ model/cct-results.php
@@ -8,31 +8,31 @@ require_once 'connection.php';
  */
 function getResultByQuizId()
 {
-  $result_arr = null;
-  if ($_GET['itemsWithAnswer']) {
-    $user_answers_str = $_GET['itemsWithAnswer'];
-    $user_arr = explode("&", $user_answers_str); // since using serialize() not serializeArray()
-    $sql = "";
-    for ($i = 0; $i < count($user_arr); $i++) {
-      $sql .= "(SELECT\n"
-        . " \"" . substr($user_arr[$i], 0, strpos($user_arr[$i], '=')) . "\" AS quizId,\n"
-        . " (SELECT COUNT(*) FROM `quizquestion`\n"
-        . "  WHERE `quizId` = \"" . substr($user_arr[$i], 0, strpos($user_arr[$i], '=')) . "\"\n"
-        . "  AND answer = " . substr($user_arr[$i], strpos($user_arr[$i], '=') + 1) . ")\n"
-        . " AS isCorrect, answer\n"
-        . " AS correctAnswer\n"
-        . " FROM `quizquestion`\n"
-        . " WHERE `quizId` = \"" . substr($user_arr[$i], 0, strpos($user_arr[$i], '=')) . "\")\n";
-      if ($i < count($user_arr) - 1) {
-        $sql .= "UNION ALL\n";
+   $result_arr = null;
+   if ($_GET['itemsWithAnswer']) {
+      $user_answers_str = $_GET['itemsWithAnswer'];
+      $user_arr = explode("&", $user_answers_str); // since using serialize() not serializeArray()
+      $sql = "";
+      for ($i = 0; $i < count($user_arr); $i++) {
+         $sql .= "(SELECT\n"
+            . " \"" . substr($user_arr[$i], 0, strpos($user_arr[$i], '=')) . "\" AS quizId,\n"
+            . " (SELECT COUNT(*) FROM `quizquestion`\n"
+            . "  WHERE `quizId` = \"" . substr($user_arr[$i], 0, strpos($user_arr[$i], '=')) . "\"\n"
+            . "  AND answer = " . substr($user_arr[$i], strpos($user_arr[$i], '=') + 1) . ")\n"
+            . " AS isCorrect, answer\n"
+            . " AS correctAnswer\n"
+            . " FROM `quizquestion`\n"
+            . " WHERE `quizId` = \"" . substr($user_arr[$i], 0, strpos($user_arr[$i], '=')) . "\")\n";
+         if ($i < count($user_arr) - 1) {
+            $sql .= "UNION ALL\n";
+         }
       }
-    }
-    $dbh = Db::getInstance();
-    $stmt = $dbh->prepare($sql);
-    $stmt->execute();
-    $result_arr = $stmt->fetchAll(PDO::FETCH_ASSOC);
-  }
-  return $result_arr;
+      $dbh = Db::getInstance();
+      $stmt = $dbh->prepare($sql);
+      $stmt->execute();
+      $result_arr = $stmt->fetchAll(PDO::FETCH_ASSOC);
+   }
+   return $result_arr;
 }
 
 echo json_encode(getResultByQuizId());
