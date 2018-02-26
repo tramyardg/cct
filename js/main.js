26132c17034e690ca3654fa831ecca50bb6c1eb5
Raymart De Guzman <tramyardg@gmail.com>
2018-02-26 18:56:19 -0500

    Update main.js
    update ordering in main js to make it more understandable

--- js/main.js
+++ js/main.js
@@ -1,16 +1,18 @@
 $(document).ready(function () {
-  TestOptions.setTestOptionsConstruct('#test-options-form')
-  TestOptions.onSubmitTestOptions()
+  // init elements
+  CustomAlert.setId('#customAlert')
   Timer.setTimerConstruct({
     timerBox: '#timer-box',
     secHTML: '#seconds-span',
     minHTML: '#minutes-span',
     radioButtons: '.q-option,input[type=radio]'
   })
-  CustomAlert.setId('#customAlert')
-  QCatalog.setQuestionForm('#questions-form')
   EnlargeImage.setConstruct({
     imageOverlay: '.image-overlay',
     imageOverlayClose: '.image-overlay-close'
   })
+  // -------------------------------------------------
+  TestOptions.setTestOptionsConstruct('#test-options-form')
+  TestOptions.onSubmitTestOptions()
+  QCatalog.setQuestionForm('#questions-form')
 })
