c244f0cc64ab43fb52020cfffc939d7ccc88d838
Raymart <tramyardg@gmail.com>
2018-03-02 00:19:03 -0500

    Enlarge image to its own file

---
+++ js/enlarge-image.js
@@ -0,0 +1,19 @@
+const EnlargeImage = {
+  imageOverlay: null,
+  imageOverlayClose: null,
+  setConstruct (args) {
+    EnlargeImage.imageOverlay = args.imageOverlay
+    EnlargeImage.imageOverlayClose = args.imageOverlayClose
+  },
+  clickToEnlarge (ele) {
+    let imageSource = $(ele).attr('src')
+    $(EnlargeImage.imageOverlay).find('img').attr('src', imageSource)
+    $(EnlargeImage.imageOverlay).fadeIn(100)
+    EnlargeImage.closeImageOverlay()
+  },
+  closeImageOverlay () {
+    $(EnlargeImage.imageOverlayClose).click(function () {
+      $(EnlargeImage.imageOverlay).fadeOut(100)
+    })
+  }
+}
