887a6f056d0dc1b8d019240eff924a3c64deaca3
Raymart De Guzman <tramyardg@gmail.com>
2018-05-30 11:46:41 -0400

    #19 loader is created via js

--- js/internal/main.js
+++ js/internal/main.js
@@ -1,5 +1,6 @@
 /* eslint-disable no-undef */
 $(document).ready(function () {
+  $('.loader-io').hide();
   // init elements
   CustomAlert.setId('#customAlert');
   Timer.setTimerConstruct({
@@ -23,11 +24,12 @@ $(document).ready(function () {
     timeTaken: {sel: '#time-taken', minSel: Timer.minutesHTML, secSel: Timer.secondsHTML}
   });
 });
-const loaderIo = $('.loader-io');
-loaderIo.hide();
+const loaderFunc = function () {
+  return '<svg width="10%" height="10%" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid" class="lds-reload"><g transform="rotate(318 50 50)"><path d="M50 15A35 35 0 1 0 74.787 25.213" fill="none" ng-attr-stroke="{{config.color}}" ng-attr-stroke-width="{{config.width}}" stroke="#5699d2" stroke-width="12"></path><path ng-attr-d="{{config.darrow}}" ng-attr-fill="{{config.color}}" d="M49 3L49 27L61 15L49 3" fill="#5699d2"></path><animateTransform attributeName="transform" type="rotate" calcMode="linear" values="0 50 50;360 50 50" keyTimes="0;1" dur="1s" begin="0s" repeatCount="indefinite"></animateTransform></g></svg>';
+};
 $(document)
   .ajaxStart(function () {
-    loaderIo.show();
+    $('.loader-io').append(loaderFunc()).show();
   }).ajaxStop(function () {
-    loaderIo.hide();
+    $('.loader-io').hide();
   });
