d978095865dc8ba47fefabafd76cb0bf699b0733
Raymart De Guzman <tramyardg@gmail.com>
2018-07-03 21:07:41 -0400

    no more util js

--- js/internal/custom-alert.js
+++ js/internal/custom-alert.js
@@ -1,4 +1,22 @@
 /* eslint-disable no-undef */
+const StrResources = {
+  timeIsUp: 'Time is up!',
+  pleaseSubmitMsg: 'Please submit the test!',
+  info: 'info',
+  warning: 'warning',
+  danger: 'danger',
+  success: 'success',
+  submitted: 'This quiz has been submitted.',
+  resultDisplayed: 'The result will be displayed in a moment. Please wait.',
+  noAnswered: 'No questions answered',
+  display: 'display',
+  block: 'block',
+  disabled: 'disabled',
+  none: 'none',
+  pleaseAnswerSome: 'Please answer some question before submitting the quiz',
+  answeredSoFar: 'Question(s) answered so far: ',
+  currentSessionIncomplete: 'The current quiz session is not completed.'
+};
 const INCREMENT_SECONDS_BY_1000 = 1000;
 const CustomAlert = { // utility not dependent on other module outside this file
   id: null,
@@ -30,28 +48,10 @@ const CustomAlert = { // utility not dependent on other module outside this file
   },
   getTimeIsUpAlert () {
     return CustomAlert.displayAlert(
-      Str.warning,
-      Str.timeIsUp,
-      Str.pleaseSubmitMsg,
+      StrResources.warning,
+      StrResources.timeIsUp,
+      StrResources.pleaseSubmitMsg,
       60
     );
   }
-};
-const Str = {
-  timeIsUp: 'Time is up!',
-  pleaseSubmitMsg: 'Please submit the test!',
-  info: 'info',
-  warning: 'warning',
-  danger: 'danger',
-  success: 'success',
-  submitted: 'This quiz has been submitted.',
-  resultDisplayed: 'The result will be displayed in a moment. Please wait.',
-  noAnswered: 'No questions answered',
-  display: 'display',
-  block: 'block',
-  disabled: 'disabled',
-  none: 'none',
-  pleaseAnswerSome: 'Please answer some question before submitting the quiz',
-  answeredSoFar: 'Question(s) answered so far: ',
-  currentSessionIncomplete: 'The current quiz session is not completed.'
 };
